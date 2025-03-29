5fbe2e40f3ee9bddac2f68efb6f14ea7ba00b0f6
ntahn <64834486+ntahn@users.noreply.github.com>
2025-03-29 16:28:49 +0700

    Update Accomplishments description

--- src/components/Accomplishments.tsx
+++ src/components/Accomplishments.tsx
@@ -7,7 +7,7 @@ export const Accomplishments: React.FC<Props> = () => {
     <div className="mx-14 my-14 space-y-4 ">
       <h1 className="text-header font-extrabold text-4xl mb-8">Accomplishments</h1>
       <p className="">
-        &#8226; Second Informatics prizes on City and Provincial levels
+        &#8226; Achieved Second Prize in competitive City and Provincial High School Informatics events, competed on National Level.
       </p>
       <p className="">
         &#8226; Top 30 Tiki Hacking Trail 2022
